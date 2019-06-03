43ea8bcb1da3b51dfcdc2a3036d584272aab1834
MernaRezkTawfik <mernarezk96@gmail.com>
2019-06-03 21:54:18 +0200

    First commit

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,47 @@
+﻿using CalendarSystem.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Calendar.Controllers
+{
+    public class CalendarController : Controller
+    {
+        #region Fields
+        private IAppointmentService _AppointmentService = null;
+        private IMonthService _MonthService = null;
+        #endregion
+        #region Constructors
+       
+        public CalendarController(IAppointmentService appointmentService,IMonthService monthService)
+        {
+            _AppointmentService = appointmentService;
+            _MonthService = monthService;
+           
+          
+        }
+        #endregion
+        // GET: Calendar
+        public ActionResult Index()
+        {
+          
+            return View();
+        }
+        public ActionResult GetMonth()
+        {
+            var Months = _MonthService.GetMonths();
+            return View(Months);
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="calendarsystem.MvcApplication" Language="C#" %>
